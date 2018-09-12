32604dd867dc96836813960f73dd77a85dcd1b13
imkopkap <imkopkap@gmail.com>
2018-09-12 17:05:19 +0700

    update

--- 2.send-and-receive/2.send-and-receive.c
+++ 2.send-and-receive/2.send-and-receive.c
@@ -9,14 +9,10 @@ int main ( int argc, char *argv[] )
 	int master = 0;
 	int slave = 1;
 	MPI_Status status;
-	//  Write an MPI Program with two proceses working as follows:
-	//	1.Process 0 sends and integer number to process 1
-	//	2.Process 1 calculates the square of the number, and sends the result to process 0
-	//	3.Process 0 prints out the result
+	
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
-
 	if ( rank == 0 ) {
 		msg = 10;
 		MPI_Send(&msg, 1, MPI_INT, slave, tag, MPI_COMM_WORLD);
